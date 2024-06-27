0c0674d511959e223a0ebe3d3eb6178e05bdbbbe
Jungbin Jung <lollin0@naver.com>
2024-06-28 00:07:00 +0900

    chore: APIConstants 생성 #2

---
+++ DOTCHI/DOTCHI/Networks/Essentials/APIConstants.swift
@@ -0,0 +1,14 @@
+//
+//  APIConstants.swift
+//  DOTCHI
+//
+//  Created by Jungbin on 6/28/24.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let baseURL = "" // ServiceEnvironment.BASE_URL
+    static let facebookAppId = "" // ServiceEnvironment.FACEBOOK_APP_ID
+    static let pagingDefaultValue: Int = 999999999
+}
