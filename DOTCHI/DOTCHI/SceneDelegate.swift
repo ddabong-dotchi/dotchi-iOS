db430a6c3e1fcd07ab850c383b8d5339f2e2f05f
Jungbin Jung <lollin0@naver.com>
2024-06-27 23:55:20 +0900

    chore: window background color 변경 #2

--- DOTCHI/DOTCHI/SceneDelegate.swift
+++ DOTCHI/DOTCHI/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        window?.backgroundColor = .systemBackground
+        window?.backgroundColor = .dotchiBlack
         window?.rootViewController = SplashViewController()
         window?.makeKeyAndVisible()
     }
