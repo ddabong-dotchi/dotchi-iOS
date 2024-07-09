45af505c60b441f6f19b278bf96830208d0cb7cf
Jungbin Jung <lollin0@naver.com>
2024-07-10 00:28:45 +0900

    create: 회원가입 뷰컨트롤러 생성 #6

---
+++ DOTCHI/DOTCHI/Sources/Screens/Signup/SignupUserInfoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignupUserInfoViewController.swift
+//  DOTCHI
+//
+//  Created by Jungbin on 7/10/24.
+//
+
+import UIKit
+import SnapKit
+
+final class SignupUserInfoViewController: BaseViewController {
+    
+    // MARK: UIComponents
+    
+    
+    
+    // MARK: Properties
+    
+    
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setLayout()
+    }
+    
+    // MARK: Methods
+    
+    
+}
+
+// MARK: - Layout
+
+extension SignupUserInfoViewController {
+    private func setLayout() {
+        self.view.addSubviews([])
+    }
+}
