a9f689276533f62d7782966e941498f09a70ae32
Jungbin Jung <lollin0@naver.com>
2024-07-11 01:00:33 +0900

    chore: APIConstants 주석 해제 및 baseURL 설정 #6

--- DOTCHI/DOTCHI/Networks/Essentials/APIConstants.swift
+++ DOTCHI/DOTCHI/Networks/Essentials/APIConstants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct APIConstants {
-    static let baseURL = "" // ServiceEnvironment.BASE_URL
-    static let facebookAppId = "" // ServiceEnvironment.FACEBOOK_APP_ID
+    static let baseURL = ServiceEnvironment.BASE_URL
+    static let facebookAppId = ServiceEnvironment.FACEBOOK_APP_ID
     static let pagingDefaultValue: Int = 999999999
 }
