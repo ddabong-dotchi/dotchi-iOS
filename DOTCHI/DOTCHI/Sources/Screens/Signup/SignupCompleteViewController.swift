793278e53cad4540975a72b1108c5fcf68f98e51
Jungbin Jung <lollin0@naver.com>
2024-08-25 17:42:12 +0900

    feat: 회원가입 - 완료 UI 구현 #18

--- DOTCHI/DOTCHI/Sources/Screens/Signup/SignupCompleteViewController.swift
+++ DOTCHI/DOTCHI/Sources/Screens/Signup/SignupCompleteViewController.swift
@@ -10,9 +10,46 @@ import SnapKit
 
 final class SignupCompleteViewController: BaseViewController {
     
+    private enum Text {
+        static let title = "축하합니다!\n따봉도치의 행운을 받았습니다"
+        static let lucky = "행운"
+        static let description = "나만의 따봉도치를 만들어 행운을 주고받아 보세요!"
+        static let start = "시작하기"
+    }
+    
     // MARK: UIComponents
     
+    private let imageView = {
+        let imageView = UIImageView(image: .imgSignupComplete)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
     
+    private let titleLabel = {
+        let label = UILabel()
+        label.font = .head
+        label.textColor = .dotchiWhite
+        label.text = Text.title
+        label.setColor(to: Text.lucky, with: .dotchiGreen)
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let descriptionLabel = {
+        let label = UILabel()
+        label.font = .sub
+        label.textColor = .dotchiLgray
+        label.text = Text.description
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let startButton: DoneButton = {
+        let button: DoneButton = DoneButton(type: .system)
+        button.setTitle(Text.start, for: .normal)
+        return button
+    }()
     
     // MARK: Properties
     
@@ -47,6 +84,34 @@ final class SignupCompleteViewController: BaseViewController {
 
 extension SignupCompleteViewController {
     private func setLayout() {
-        self.view.addSubviews([])
+        self.view.addSubviews([
+            imageView,
+            titleLabel,
+            descriptionLabel,
+            startButton
+        ])
+        
+        self.imageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(100)
+            make.width.equalToSuperview()
+            make.height.equalTo(self.imageView.snp.width).multipliedBy(0.648854)
+        }
+        
+        self.titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.imageView.snp.bottom).offset(27)
+            make.horizontalEdges.equalToSuperview()
+        }
+        
+        self.descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(20)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(14)
+        }
+        
+        self.startButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(32)
+            make.horizontalEdges.equalToSuperview().inset(28)
+            make.height.equalTo(52)
+        }
     }
 }
