e0313d538a1d7720a8ca6cb599be19cea6e5bed3
Jungbin Jung <lollin0@naver.com>
2024-08-29 01:00:20 +0900

    chore: 탭바 - 홈뷰컨 연결 #20

--- DOTCHI/DOTCHI/Sources/Screens/Home/HomeViewController.swift
+++ DOTCHI/DOTCHI/Sources/Screens/Home/HomeViewController.swift
@@ -12,7 +12,10 @@ final class HomeViewController: BaseViewController {
     
     // MARK: UIComponents
     
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
     
+    private let todayView = HomeTodayView()
     
     // MARK: Properties
     
@@ -24,10 +27,14 @@ final class HomeViewController: BaseViewController {
         super.viewDidLoad()
         
         self.setLayout()
+        self.setUI()
     }
     
     // MARK: Methods
     
+    private func setUI() {
+        self.view.backgroundColor = .dotchiHomeBackgroundGray
+    }
     
 }
 
@@ -35,6 +42,24 @@ final class HomeViewController: BaseViewController {
 
 extension HomeViewController {
     private func setLayout() {
-        self.view.addSubviews([])
+        self.view.addSubviews([scrollView])
+        self.scrollView.addSubviews([contentView])
+        self.contentView.addSubviews([
+            todayView
+        ])
+        
+        self.scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(self.view.safeAreaLayoutGuide)
+        }
+        
+        self.contentView.snp.makeConstraints { make in
+            make.edges.width.equalToSuperview()
+        }
+        
+        self.todayView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(451)
+        }
     }
 }
