209d28c2d1e576e0c40aacb3329cfa52ee7c7966
YuBin Kim <yubin412@naver.com>
2024-09-09 00:32:45 +0900

    feat: 로그아웃 Service, Router 작성 #22

--- DOTCHI/DOTCHI/Networks/Services/AuthService.swift
+++ DOTCHI/DOTCHI/Networks/Services/AuthService.swift
@@ -36,4 +36,20 @@ extension AuthService: AuthServiceProtocol {
             }
         }
     }
+    
+    // [GET] 로그아웃
+    
+    func logout(completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        self.provider.request(.logout) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                let networkResult = self.judgeStatus(by: statusCode, data, String.self)
+                completion(networkResult)
+            case .failure(let error):
+                debugPrint(error)
+            }
+        }
+    }   
 }
