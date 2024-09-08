38973f3547cd5a8c2d9758eb3a23e27050aa4010
Jungbin Jung <lollin0@naver.com>
2024-09-08 15:40:31 +0900

    chore: Lottie 애니메이션 구현 #20

--- DOTCHI/DOTCHI/Sources/Components/UIView/HomeTodayView.swift
+++ DOTCHI/DOTCHI/Sources/Components/UIView/HomeTodayView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import Lottie
 
 final class HomeTodayView: UIView {
     
@@ -32,6 +33,33 @@ final class HomeTodayView: UIView {
         return label
     }()
     
+    private let dateStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 7
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private let graphImageView = {
+        let imageView = UIImageView(image: .imgGraph)
+        return imageView
+    }()
+    
+    private let dateLabel = {
+        let label = UILabel()
+        label.font = .sub
+        label.textColor = .dotchiWhite
+        return label
+    }()
+    
+    private var particleAnimationView = {
+        let view: LottieAnimationView = LottieAnimationView(name: "pung")
+        view.loopMode = .playOnce
+        view.animationSpeed = 0.8
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
     // MARK: Initializer
     
     override init(frame: CGRect) {
@@ -47,7 +75,29 @@ final class HomeTodayView: UIView {
         
         if self.superview != nil {
             self.setLayout()
+            self.setUI()
         }
+    }
+    
+    // MARK: Methods
+    
+    private func setUI() {
+        self.dateLabel.text = self.generateDateText()
+        self.setParticleAnimationView()
+    }
+    
+    private func setParticleAnimationView() {
+        self.particleAnimationView.play()
+    }
+    
+    private func generateDateText() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년 M월 d일"
+
+        let currentDate = Date()
+        let formattedDate = dateFormatter.string(from: currentDate)
+
+        return "\(formattedDate) 기준"
     }
 }
 
@@ -55,9 +105,16 @@ final class HomeTodayView: UIView {
 
 extension HomeTodayView {
     private func setLayout() {
+        self.dateStackView.addArrangedSubviews([
+            graphImageView,
+            dateLabel
+        ])
+        
         self.addSubviews([
             logoView,
-            titleLabel
+            titleLabel,
+            dateStackView,
+            particleAnimationView
         ])
         
         self.logoView.snp.makeConstraints { make in
@@ -70,6 +127,18 @@ extension HomeTodayView {
         self.titleLabel.snp.makeConstraints { make in
             make.top.equalTo(self.logoView.snp.bottom).offset(51.47)
             make.centerX.equalToSuperview()
+        }
+        
+        self.dateStackView.snp.makeConstraints { make in
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(14)
+        }
+        
+        self.particleAnimationView.snp.makeConstraints { make in
+            make.top.equalTo(self.logoView.snp.bottom)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(200)
         }
     }
 }
