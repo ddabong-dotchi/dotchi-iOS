a1933ee0c08532b2af4bb154feb5d8a8a63d3ae7
YuBin Kim <yubin412@naver.com>
2024-09-30 01:04:59 +0900

    chore: MyCardResponseDTO 수정 #28

--- DOTCHI/DOTCHI/Networks/DTO/User/Response/MyCardResponseDTO.swift
+++ DOTCHI/DOTCHI/Networks/DTO/User/Response/MyCardResponseDTO.swift
@@ -13,4 +13,23 @@ struct MyCardResponseDTO: Codable {
     let title: String
     let type: String
     let imageUrl: String
+
+    enum CodingKeys: String, CodingKey {
+        case username, cardId, title, type, imageUrl
+    }
+}
+
+typealias MyCardResponseDTOList = [MyCardResponseDTO]
+
+extension MyCardResponseDTOList {
+    func toCardFrontEntity() -> [CardFrontEntity] {
+        return self.map { card in
+            CardFrontEntity(
+                cardId: card.cardId,
+                imageUrl: card.imageUrl,
+                luckyType: LuckyType(rawValue: card.type) ?? .lucky,
+                dotchiName: card.title
+            )
+        }
+    }
 }
